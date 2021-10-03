0daea1fbbe049bc53f7670323b9aa3f3447394a3
Amber Lehua <46366315+amberlehua@users.noreply.github.com>
2021-10-02 18:29:11 -1000

    Fetch repo data

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 //This div is where my profile information will appear
 const overview = document.querySelector(".overview");
 const username = "amberlehua";
-const display = document.querySelector("ul")
-
+const display = document.querySelector(".repo-list")
 
 const gitUserInfo = async function () {
     const userInfo = await fetch (`https://api.github.com/users/${username}`);
@@ -25,7 +24,22 @@ const displayInfo = function (data) {
       <p><strong>Location:</strong> ${data.location}</p>
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div> 
-    `
-    ;
+    `;
     overview.append(div);
-}
+    gitRepoInfo();
+};
+
+const gitRepoInfo = async function () {
+  const repoInfo = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`)
+  const repoData = await repoInfo.json();
+  repoDisplay(repoData);
+};
+
+const repoDisplay = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`
+    display.append(repoItem);
+  }
+};
