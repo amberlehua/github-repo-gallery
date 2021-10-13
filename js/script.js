89af7fffedad6da32b5fb9a892129acaa280b806
Amber Lehua <46366315+amberlehua@users.noreply.github.com>
2021-10-12 21:22:08 -1000

    Display repo info

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 //This div is where my profile information will appear
 const overview = document.querySelector(".overview");
 const username = "amberlehua";
-const display = document.querySelector(".repo-list")
+const repoList = document.querySelector(".repo-list");
+const repoInfoDisplay = document.querySelector(".repos");
+const repoDataDisplay = document.querySelector(".repo-data");
+
+
 
 const gitUserInfo = async function () {
-    const userInfo = await fetch (`https://api.github.com/users/${username}`);
-    const data = await userInfo.json();
-    displayInfo(data)
+  const userInfo = await fetch (`https://api.github.com/users/${username}`);
+   const data = await userInfo.json();
+   displayInfo(data)
 };
 
 gitUserInfo()
 
 const displayInfo = function (data) {
     const div = document.createElement("div");
-    div.classList.add = ("user-info");
+    div.classList.add("user-info");
     div.innerHTML = `
     <figure>
       <img alt="user avatar" src=${data.avatar_url} />
@@ -40,6 +44,48 @@ const repoDisplay = function (repos) {
     const repoItem = document.createElement("li");
     repoItem.classList.add("repo");
     repoItem.innerHTML = `<h3>${repo.name}</h3>`
-    display.append(repoItem);
+    repoList.append(repoItem);
   }
+};
+
+// At the bottom of your code, create an event listener called repoList for a click event on the unordered list 
+//  with a class of “repo-list.” Pass the event (e) in the callback function. 
+
+repoList.addEventListener ("click", function (e) {
+  if (e.target.matches("h3")) {
+    const repoName = e.target.innerText;
+    getRepoInfo(repoName);
+  }
+});
+
+const getRepoInfo = async function (repoName) {
+  const fetchInfo = await fetch (`https://api.github.com/repos/${username}/${repoName}`);
+  const repoInfo = await fetchInfo.json();
+ 
+  //Grab languages
+  const fetchLanguages = await fetch (`https://api.github.com/repos/${username}/${repoName}/languages`)
+  const languageData = await fetchLanguages.json();
+
+  //Make a list of languages
+  const languages = [];
+  for (let language in languageData) {
+    languages.push(language);
+  }
+
+  displayRepoInfo(repoInfo,languages);
+};
+
+  const displayRepoInfo = function (repoInfo, languages) {
+    repoDataDisplay.innerHTML = ""
+    repoDataDisplay.classList.remove("hide");
+    repoInfoDisplay.classList.add("hide");
+    const div = document.createElement("div");
+    div.innerHTML =   `
+      <h3>Name: ${repoInfo.name}</h3>
+      <p>Description: ${repoInfo.description}</p>
+      <p>Default Branch: ${repoInfo.default_branch}</p>
+      <p>Languages: ${languages.join(", ")}</p>
+      <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+    `;
+  repoDataDisplay.append(div);
 };
